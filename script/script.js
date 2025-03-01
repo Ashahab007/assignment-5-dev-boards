59cc9c62650e72da1bfeb1e07dbca315d0440380
Ashahab007 <ashahab007@gmail.com>
2025-03-01 20:00:19 +0600

    created activity log except title capturing, creating blog page

--- script/script.js
+++ script/script.js
@@ -2,6 +2,13 @@
 let titleDate = new Date();
 document.getElementById("title-date").innerHTML = titleDate.toDateString();
 
+// acivitylog function
+/* function acivitylogFunction() {
+  let history = document.querySelectorAll(".card h3")[0].innerText;
+  return history;
+}
+alert(acivitylogFunction()); */
+
 const card = document.querySelectorAll(".card");
 // console.log(card.length);
 let totalCounter = document.getElementById("total-counter");
@@ -18,18 +25,30 @@ for (let i = 0; i < card.length; i++) {
   //   taskCounter.innerText = convertedTaskCounter;
   let btnComplete = document.getElementsByClassName("btn-complete")[i];
   btnComplete.addEventListener("click", function (event) {
+    console.log(event.target.querySelector(".card"));
+
     alert("Board updated successfully");
     event.target.setAttribute("disabled", false);
 
     let activityLogMessage = document.getElementById("activity-log-message");
+    let p = document.createElement("p");
+    p.classList.add("bg-primary");
+    p.classList.add("p-2");
+    p.classList.add("mt-3");
+    p.innerHTML = `
+    You have Complete The Task at ${logTime()}
+    `;
+    // let history = event.target;
+    // console.log(history);
+    // console.log(activityLogMessage);
+
     if (event.target.getAttribute("disabled") === "false") {
       // taskCounter.innerText = convertedTaskCounter - 1;
       taskCounter.innerText -= 1;
       console.log(typeof taskCounter.innerText);
       convertedTotCounter += 1;
-      // console.log(totalCounter);
-
       totalCounter.innerText = convertedTotCounter;
+      activityLogMessage.appendChild(p);
       if (taskCounter.innerText === "0") {
         alert("Congratulations!!! You have completed all the current task");
       }
@@ -39,3 +58,33 @@ for (let i = 0; i < card.length; i++) {
     //   console.log(typeof convertedTaskCounter);
   });
 }
+
+// clear history button
+document
+  .getElementById("btn-clear-history")
+  .addEventListener("click", function () {
+    document.getElementById("activity-log-message").innerHTML = "";
+  });
+
+// activity log time
+function logTime() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let time = `${hours} : ${minutes} : ${seconds}`;
+  return time;
+}
+
+// discover button
+document.getElementById("btn-discover").addEventListener("click", function () {
+  window.location.href = "./blog.html";
+});
+
+// Back to desk button
+
+document
+  .getElementById("btn-backToDesk")
+  .addEventListener("click", function () {
+    window.location.href = "./index.html";
+  });
